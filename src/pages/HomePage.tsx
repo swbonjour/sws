77c11fe08409de1db7d71f70cb573cda55f8541a
swbonjour <71038541+swbonjour@users.noreply.github.com>
2023-01-31 22:16:16 +0300

    sidebar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { AppHeader } from 'components/AppHeader/AppHeader'
+import { AppSidebar } from 'components/AppSidebar/AppSidebar'
 
 export const HomePage = () => {
   return (
     <div>
         <AppHeader></AppHeader>
+        <AppSidebar></AppSidebar>
     </div>
   )
 }
